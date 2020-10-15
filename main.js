1bff16f14b8f3d1b8b8302ba34bc9285742e1f12
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-15 15:26:48 -0400

    Enter and submit name to update student name

--- main.js
+++ main.js
@@ -43,7 +43,16 @@ function onLoadStudentInfo() {
   setGrades();
 };
 
+function updateStudentName() {
+  let nameInput = document.querySelector('.name-input');
+  let updatedStudentName = nameInput.value;
+  nameInput.value = '';
+  studentReportCard.name = updatedStudentName;
+  setStudentName();
+
+}
+
+document.addEventListener('click', updateStudentName);
+
 // Looking for extra work?
-// 1. onLoadStudentInfo is one big function being called on page load! Is there a way we can refactor so the onLoadStudentInfo is not as big?
-// 2. Make a button that accepts a new student name from the user. Make a function that first updates the studentReportCard object and then resets the html on the report card with the new student name.
 //3. Extra challenge! How could you change the data model to hold the class name and the grade (i.e. "Physics" and "D"). How could you loop through the class list and update both the class name and the grade. Hint: you'll need to use the ids of the li elements to access the corresponding studentReportCard properties. You'll also have to look up some ways to change elements inside elements.
